1f6c3e50f1491ec4d005cf4aae98df005ea09437
Kristian Koivisto <kristian.koivisto@iki.fi>
2020-06-18 19:00:56 +0300

    1

--- src/app/d15-view/d15-view.component.ts
+++ src/app/d15-view/d15-view.component.ts
@@ -2,6 +2,12 @@ import { Component, OnInit, AfterViewInit, ViewChild, ElementRef, Renderer2 } fr
 import * as d3 from 'd3';
 import { line } from 'd3';
 
+
+type myData = {
+  name: string,
+  value: number
+};
+
 @Component({
   selector: 'app-d15-view',
   templateUrl: './d15-view.component.html',
@@ -11,20 +17,34 @@ export class D15ViewComponent implements OnInit, AfterViewInit {
 
   @ViewChild('chart') private chartContainer: ElementRef;
 
+  data: myData[] = [];
 
-
-  xScale = d3.scaleLinear().domain([0, 10]).range([0, 2000]);
+  yScale = d3.scaleLinear().domain([1, 0]).range([0, 200]);
 
   x2Scale = d3.scaleBand()
     .domain(['Liikuntakyky', 'Näkö', 'Kuulo', 'Hengitys', 'Nukkuminen', 'Syöminen', 'Puhe', 'Eritys',
       'Tavanomaiset toiminnot', 'Henkinen toiminta', 'Vaivat ja oireet', 'Masentuineisuus',
       'Ahdistuneisuus', 'Energisyys', 'Sukupuolielämä'])
-    .range([0, 2000]);
+    .range([0, 800]);
 
   constructor(private renderer: Renderer2) { }
 
   ngOnInit(): void {
-
+    this.data.push({ name: 'Liikuntakyky', value: 0.8 });
+    this.data.push({ name: 'Näkö', value: 0.7 });
+    this.data.push({ name: 'Kuulo', value: 0.3 });
+    this.data.push({ name: 'Hengitys', value: 0.5 });
+    this.data.push({ name: 'Nukkuminen', value: 0.4 });
+    this.data.push({ name: 'Syöminen', value: 0.8 });
+    this.data.push({ name: 'Puhe', value: 1.00 });
+    this.data.push({ name: 'Eritys', value: 0.8 });
+    this.data.push({ name: 'Tavanomaiset toiminnot', value: 0.3 });
+    this.data.push({ name: 'Henkinen toiminta', value: 0.5 });
+    this.data.push({ name: 'Vaivat ja oireet', value: 0.4 });
+    this.data.push({ name: 'Masentuineisuus', value: 0.8 });
+    this.data.push({ name: 'Ahdistuneisuus', value: 0.5 });
+    this.data.push({ name: 'Energisyys', value: 0.4 });
+    this.data.push({ name: 'Sukupuolielämä', value: 0.8 });
   }
 
   ngAfterViewInit(): void {
@@ -32,32 +52,32 @@ export class D15ViewComponent implements OnInit, AfterViewInit {
 
     const svg = d3.select(element)
       .append('svg')
-      .attr('width', element.offsetWidth)
+      .attr('width', element.offsetWidth / 2)
       .attr('height', 300);
 
 
-    const lineGenerator = d3.line();
+    const lineGenerator = d3.line<myData>();
 
     lineGenerator.x((d, i) => {
-      return this.xScale(i);
+      return this.x2Scale(d.name);
     });
 
-    const pathData = lineGenerator([
-      [0, 100],
-      [100, 80],
-      [200, 30],
-      [300, 50],
-      [400, 40],
-      [500, 80]
-    ]);
+    lineGenerator.y((d, i) => {
+      return this.yScale(d.value);
+    });
+
+    const pathData = lineGenerator(this.data);
 
     const path = svg.append('path');
-    path.attr('d', pathData).style('fill', 'none').style('stroke', '#999');
+    path.attr('d', pathData).style('fill', 'none').style('stroke', '#999').attr('transform', 'translate(65,0)');
 
 
     const xAxis = d3.axisBottom(this.x2Scale);
-    xAxis.scale(this.x2Scale);
-    svg.append('g').call(xAxis);
+    svg.append('g').call(xAxis).attr('transform', 'translate(0, 220)').selectAll('text').attr('transform', 'rotate(-45)')
+      .style("text-anchor", "end");
+
+    const yAxis = d3.axisLeft(this.yScale);
+    svg.append('g').call(yAxis).attr('transform', 'translate(30, 0)');
 
   }
 }
